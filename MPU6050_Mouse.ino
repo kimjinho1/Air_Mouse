d30ee5ea2b6c48366fd24fefb8c0bc45cb2bfdf5
jinho <rlawlsgh8113@naver.com>
2019-08-12 23:18:58 +0900

    Add files via upload

--- MPU6050_Mouse.ino
+++ MPU6050_Mouse.ino
@@ -1,23 +1,40 @@
-#include<Mouse.h> // 마우스 움직여야지!
 #include<Wire.h> // 통신? 때문에 필요
+#include<Mouse.h> // 마우스 움직여야지!
+
+
+/*
+ SDA 2번 Pin, SCL 3번 Pin
+ Int 7번 Pin
+ Int 무조건 박아야됨. 
+ */
+
+
+#define MPU6050_INT_PIN 7
+#define MPU6050_INT digitalPinToInterrupt(MPU6050_INT_PIN)
 
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ,gyroX,gyroZ; // 센서 출력값, Tmp는 온도임.
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 int sensitivity = 2000; /* 출력 값이 엄청 커서 그대로 Mouse.move로 들어가면 마우스 움직임 폭이 너무 넓어서 조절이 안됨.
                              그래서 큰 숫자를 나눠줌. 딱 쓰기 편할 정도로 조절해야 할 듯. 숫자 바꿔가면서 노가다 ㄱㄱ*/
                              
 void setup()
 {
   // 설정인데 안 건드는게 좋음. 잘 되있음.
-  // Mouse.begin() 넣어야 되는줄 알았더니 이거 넣으니까 계속 업로드가 안되는거 같음. 일단 지우고 하면 잘되니까 냅두자
+  Serial.begin(9600);
+
+  Serial.println("start");
+  Serial.print(MPU6050_INT); Serial.print(" on pin "); Serial.print(MPU6050_INT_PIN);
+  
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  pinMode(2, INPUT); // 좌클릭 버튼
-  pinMode(4, INPUT); // 우클릭 버튼
-  Serial.begin(9600);
+  pinMode(4, INPUT); // 좌클릭 버튼
+  pinMode(8, INPUT); // 우클릭 버튼
+  Mouse.begin(); //넣어야 되는줄 알았더니 이거 넣으니까 계속 업로드가 안되는거 같음. 일단 지우고 하면 잘되니까 냅두자
+  pinMode(MPU6050_INT, INPUT);
+  //attachInterrupt(MPU6050_INT, dmpDataReady, RISdlfhgk62ING);
 }
 
 void loop()
@@ -35,26 +52,27 @@ void loop()
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
   // 여기 위까지는 코드 건들지 않는 걸 추천. 여기 아래부터 수정 ㄱㄱ
 
-  int Left_button = digitalRead(2); // 1이면 누른 상태, 0이면 안 누른 상태임.
-  int Right_button = digitalRead(4); // 위와 마찬가지
+  int lb = digitalRead(4); // 1이면 누른 상태, 0이면 안 누른 상태임.
+  int rb = digitalRead(8); // 위와 마찬가지
   
-  gyroX = -GyX / sensitivity; /* 내가 연결한 방향은 마이너스를 해야지 원하는 방향으로 움직임. 연결방식마다 다르니 자기 
+  int gyroX = - GyX / 2000; /* 내가 연결한 방향은 마이너스를 해야지 원하는 방향으로 움직임. 연결방식마다 다르니 자기 
                                  께 반대로 움직인다 싶으면 부호를 바꿔주자*/  
-  gyroZ = -GyZ / sensitivity; // 위와 마찬가지
-  Sprint(gyroX, gyroZ, Left_button, Right_button);
+  int gyroZ = - GyZ / 1800; // 위와 마찬가지
 
-  if (Left_button == 1) // Left button이 1이라는건 좌클릭을 하고 있다는 상태이므로 마우스를 누른 상태로 유지시켜주는 Mouse.press()를 사용함
+  if (lb == 1) // Left button이 1이라는건 좌클릭을 하고 있다는 상태이므로 마우스를 누른 상태로 유지시켜주는 Mouse.press()를 사용함
   {
     Mouse.press(); // 좌클릭 한 상태 유지
   }
-  else if (Left_button == 0 && Right_button == 0) 
+  else if (lb == 0 && rb == 0) 
   {
     Mouse.release(); //클릭 상태 해제
   }
-  else if (Right_button == 1) // 우클릭
+  else if (rb == 1) // 우클릭
   {
     Mouse.click(MOUSE_RIGHT);
   } 
+  
+  Sprint(gyroX, gyroZ, lb, rb);
   Mouse.move(gyroZ, gyroX); // 마우스를 움직이는 함수
 }
 
